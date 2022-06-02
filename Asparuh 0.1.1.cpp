9d7b1bac51eae10890bd3cc8279870a5804fa9ed
StefanVNikolov <84364419+StefanVNikolov@users.noreply.github.com>
2022-06-02 00:00:31 -0700

    Create Asparuh 0.1.1.cpp
    Creating a cpp file

---
+++ Asparuh 0.1.1.cpp
@@ -0,0 +1,13 @@
+//ASPARUH_0.1.1 C++ Version
+//+----------------------------------------------+
+//| This program generates a tree of chess games |
+//| given the depth of search or a time limit    |
+//+----------------------------------------------+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//Including a test
+	cout << "This is a test" << endl;
+}
