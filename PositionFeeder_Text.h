5dd361e8b3bfb0a8f182eff6fde53e5e747eee39
StefanVNikolov <84364419+StefanVNikolov@users.noreply.github.com>
2022-06-03 17:13:38 -0700

    Return update
    Now to extractInformation function returns a vector of ChessPieces
    
    *convert_coordinates is initialized twice (needs a fix)

--- PositionFeeder_Text.h
+++ PositionFeeder_Text.h
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<vector>
 #include "structs.cpp"
 
 using namespace std;
@@ -16,8 +17,11 @@ class PositionFeeder_Text
 {
 public:
 
-	void extractInformation()
+	vector<ChessPiece> extractInformation()
 	{
+        //Create ChessPieces holder vector
+        vector<ChessPiece> piecesFeed;
+
 		//Opens the text file
 		ifstream Feed(fileName);
 
@@ -29,7 +33,7 @@ public:
             PieceSpecifications specs;
             //Feed data per log (line)
             int type, color;
-            string position;
+            char position[3];
 
             //read first value
             Feed >> type;
@@ -39,13 +43,38 @@ public:
                 Feed >> color;
                 Feed >> position;
 
+                //Create a new piece
+                ChessPiece current_chess_piece;
+                current_chess_piece.exists = true;
+                current_chess_piece.type = type;
+                current_chess_piece.color = color;
+                current_chess_piece.x_coordinate = convert_coordinates(position)[0];
+                current_chess_piece.y_coordinate = convert_coordinates(position)[1];
+
+                //Add the piece to the holder vector
+                piecesFeed.push_back(current_chess_piece);
+
                 cout << specs.colors[color] << " " << specs.types[type] << " was positioned on " << position << endl;
                 //read the beginning of the next data record
                 Feed >> type;
             }
             Feed.close();
         }
+        return piecesFeed;
 	}
+    // This function takes input coordinates and converts them to 
+    // 2-dimentional array coordinates
+    short int* convert_coordinates(char* conventional_coordinates)
+    {
+        short int coordinates[2];
+        short int x_coordinate = conventional_coordinates[0] - 65;
+        short int y_coordinate = 7 - (conventional_coordinates[1] - 49);
+
+        coordinates[0] = x_coordinate;
+        coordinates[1] = y_coordinate;
+
+        return coordinates;
+    }
 
 };
 
